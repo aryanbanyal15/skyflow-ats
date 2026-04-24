12f22c9783da67d8fa06abbeffe9a387a4075703
Aryan Banyal <aryanbanyal15@gmail.com>
2026-04-24 10:53:40 -0700

    Fixed AuthContext import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from 'react';
-import { AuthContext } from '../App';
+import { AuthContext } from '../App.js';
 import { useNavigate } from 'react-router-dom';
 
 export default function Login() {
